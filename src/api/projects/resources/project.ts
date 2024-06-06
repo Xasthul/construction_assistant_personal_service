d80893b58e724069200dd801d26aa18235ab1136
nazhon <nazargon2003@gmail.com>
2024-06-07 00:54:23 +0300

    Expose users in projects resource

--- src/api/projects/resources/project.ts
+++ src/api/projects/resources/project.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger"
+import { UserResource } from "src/api/users/resources/user";
 import { Project } from "src/domain/models/project.entity";
 
 export class ProjectResource {
@@ -9,12 +10,19 @@ export class ProjectResource {
     @ApiProperty()
     readonly title: string
 
+    @ApiProperty({
+        isArray: true,
+        type: UserResource,
+    })
+    readonly users: UserResource[]
+
     @ApiProperty({ description: 'Project owner id' })
     readonly createdBy: string
 
     constructor(project: Project) {
         this.id = project.id;
         this.title = project.title;
+        this.users = project.users.map(user => UserResource.from(user));
         this.createdBy = project.createdById;
     }
 
