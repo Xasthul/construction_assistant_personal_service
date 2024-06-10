9425541e5a08d550b414fd4a6b6114fa34c78efc
nazhon <nazargon2003@gmail.com>
2024-06-11 00:34:32 +0300

    Register JwtModule in users module

--- src/api/users/users.module.ts
+++ src/api/users/users.module.ts
@@ -4,11 +4,19 @@ import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/domain/models/user.entity';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
-    JwtModule,
+    // TODO: already registered in auth. Should be shared
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('jwtSecret'),
+      }),
+      inject: [ConfigService]
+    })
   ],
   providers: [UsersService],
   controllers: [UsersController],
